c382a2b5d850ff46030934546288319bb467fbe4
Markobaboci <markobaboci@gmail.com>
2024-09-29 19:12:31 +0200

    have yet to add pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  adress: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Deluxe appartment blloku',
+  adress: 'Tek ish-Farmacia Greke',
+  description: 'Kati i fundit i nje pallati te ri, apartament me dy dhoma gjumi dhe dy banjo ideal per familje ose grup shoqeror deri ne 4 persona',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Premium Penthouse',
+  adress: 'Ish Pub RobRoy',
+  description: 'Super pamje nga kati i fundit i nje pallati te ri, apartament me nje dhome gjumi dhe dy banjo ideal per familje ose grup shoqeror deri ne 4 persona',
+  price_per_night: 90,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Family last floor',
+  adress: 'Pallatet Agimi, Blloku, Tirana',
+  description: 'Super pamje nga kati i fundit i nje pallati te ri, apartament me nje dhome gjumi dhe dy banjo ideal per familje ose grup shoqeror deri ne 4 persona',
+  price_per_night: 95,
+  number_of_guests: 4
+)
